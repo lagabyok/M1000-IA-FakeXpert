45eca4b2f5266d4f3fa1c43b968233c5f3ac73dd
gabriela <gafacrae@gmail.com>
2024-05-09 02:30:01 -0300

    estructura proyecto
    agregue carpetas para estructurar la exploración y reporte de datos

---
+++ data/raw/data.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+# Cargar los archivos CSV
+true_data = pd.read_csv(r'C:\Users\facus\OneDrive\Documents\M1000-IA-FakeXpert\data\raw\True_converted.csv')
+false_data = pd.read_csv(r'C:\Users\facus\OneDrive\Documents\M1000-IA-FakeXpert\data\raw\Fake_converted.csv')
+
+# Agregar una nueva columna indicando la fuente
+true_data['source'] = 'True'
+false_data['source'] = 'False'
+
+# Concatenar los DataFrames
+combined_data = pd.concat([true_data, false_data], ignore_index=True)
+
+# Guardar el nuevo dataset unificado en un CSV
+combined_data.to_csv(r'C:\Users\facus\OneDrive\Documents\M1000-IA-FakeXpert\data\processed\combined_data.csv', index=False)
